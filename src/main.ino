78e3b0d98a3690390f6e16fe1231e0340bddc852
faizwibawa <faizgymnastiarwibawa@mail.ugm.ac.id>
2025-07-21 22:36:26 +0700

    initial commit

---
+++ src/main.ino
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Hello, ESP32!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(10); // this speeds up the simulation
+}
